6af14606231f1b7d62d31954b522d2dee3549f21
prerakc <prerak.chaudhari@yahoo.ca>
2022-06-09 21:01:35 -0400

    Finish Programming Assignment 2

--- pa2/planner.py
+++ pa2/planner.py
@@ -11,6 +11,8 @@ import os
 from search import * 
 # Warehouse specific classes
 from warehouse import WarehouseState, Direction, warehouse_goal_state
+# My imports
+import math
 
 def heur_displaced(state):
   '''A trivial example heuristic that is admissible'''
@@ -38,6 +40,21 @@ def heur_manhattan_distance(state):
     
     ### IMPLEMENT BELOW ###
 
+    manhattan_distance_sum = 0
+
+    for box in state.boxes:
+      smallest_manhattan_distance = math.inf
+
+      for storage in state.storage:
+        manhattan_distance = abs(box[0] - storage[0]) + abs(box[1] - storage[1])
+
+        if manhattan_distance < smallest_manhattan_distance:
+          smallest_manhattan_distance = manhattan_distance
+      
+      manhattan_distance_sum += smallest_manhattan_distance
+
+    return manhattan_distance_sum
+
     ### END OF IMPLEMENTATION ###
     
     return 0
@@ -50,6 +67,19 @@ def weighted_astar(initial_state, heuristic, weight, timebound = 10):
     '''OUTPUT: A WarehouseState (if a goal is found), else False'''
     
     ### IMPLEMENT BELOW ###
+
+    wrap_fval_fn = lambda sN: sN.gval + weight * sN.hval
+
+    se = SearchEngine('custom', 'full')
+    se.init_search(
+      initial_state,
+      goal_fn=warehouse_goal_state,
+      heur_fn=heuristic,
+      fval_function=wrap_fval_fn
+    )
+    state, _ = se.search(timebound)
+
+    return state
 
     ### END OF IMPLEMENTATION ###
 
@@ -66,6 +96,34 @@ def iterative_astar(initial_state, heuristic, weight, timebound = 10):
     
     ### IMPLEMENT BELOW ###
 
+    function_start = os.times()[0]
+
+    best_state = False
+    wrap_fval_fn = lambda sN: sN.gval + weight * sN.hval
+
+    timebound -= os.times()[0] - function_start
+
+    while timebound > 0:
+      start_time = os.times()[0]
+      
+      se = SearchEngine('custom', 'full')
+      se.init_search(
+        initial_state,
+        goal_fn=warehouse_goal_state,
+        heur_fn=heuristic,
+        fval_function=wrap_fval_fn
+      )
+      state, _ = se.search(timebound)
+
+      if not state:
+        return best_state
+
+      best_state = state
+      weight = weight / 2
+      timebound -= os.times()[0] - start_time
+        
+    return best_state
+
     ### END OF IMPLEMENTATION ###
 
     return False
@@ -81,7 +139,72 @@ def heur_alternate(state):
     
     ### IMPLEMENT BELOW ###
 
+    available_storages = state.storage - state.boxes
+    boxes_left = state.boxes - state.storage
+
+    board_corners = {
+      (0, 0),
+      (state.width - 1, 0),
+      (0, state.height - 1),
+      (state.width - 1, state.height - 1)
+    }
+
+    no_available_storages_top_edge = len([s for s in available_storages if s[1] == 0]) == 0
+    no_available_storages_bottom_edge = len([s for s in available_storages if s[1] == state.height - 1])  == 0
+    no_available_storages_left_edge = len([s for s in available_storages if s[0] == 0])  == 0
+    no_available_storages_right_edge = len([s for s in available_storages if s[0] == state.width - 1])  == 0
+
+    blockers = state.boxes | state.obstacles
+
+    for box in boxes_left:
+      # check if box in board corner
+      if box in board_corners:
+        return math.inf
+
+      # check if box is on edge with no storages
+      if (box[1] == 0 and no_available_storages_top_edge) or \
+          (box[1] == state.height - 1 and no_available_storages_bottom_edge) or \
+          (box[0] == 0 and no_available_storages_left_edge) or \
+          (box[0] == state.width - 1 and no_available_storages_right_edge):
+        return math.inf
+
+      # check if box is cornered by edges or blockers
+      up = box[1] == 0 or (box[0], box[1] - 1) in blockers
+      down = box[1] == state.height - 1 or (box[0], box[1] + 1) in blockers
+      left = box[0] == 0 or (box[0] - 1, box[1]) in blockers
+      right = box[0] == state.width - 1 or (box[0] + 1, box[1]) in blockers
+
+      if (up or down) and (left or right):
+        return math.inf
+
+    robot_box_manhattan_distance_sum = 0
+
+    for robot in state.robots:
+      smallest_manhattan_distance = math.inf
+
+      for box in state.boxes:
+        manhattan_distance = abs(robot[0] - box[0]) + abs(robot[1] - box[1]) - 1
+
+        if manhattan_distance < smallest_manhattan_distance:
+          smallest_manhattan_distance = manhattan_distance
+
+      robot_box_manhattan_distance_sum += smallest_manhattan_distance
+
+    box_storage_manhattan_distance_sum = 0
+
+    for box in state.boxes:
+      smallest_manhattan_distance = math.inf
+
+      for storage in state.storage:
+        manhattan_distance = abs(box[0] - storage[0]) + abs(box[1] - storage[1])
+
+        if manhattan_distance < smallest_manhattan_distance:
+          smallest_manhattan_distance = manhattan_distance
+
+      box_storage_manhattan_distance_sum += smallest_manhattan_distance
+
+    return robot_box_manhattan_distance_sum + box_storage_manhattan_distance_sum
+
     ### END OF IMPLEMENTATION ###
     
     return 0
-
